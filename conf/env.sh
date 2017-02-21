9ad72be65d6833224bbeae6b1a36a736db09171c
Ciro Cavani <ciro.cavani@gmail.com>
2017-02-20 23:23:28 -0300

    Renomeia conda3 -> jupyter, volta a usar pip no env do tensorflow

--- conf/env.sh
+++ conf/env.sh
@@ -2,9 +2,10 @@
 set -eu
 
 export PROJECT_HOME=$(pwd)
-export CONDA_HOME=$PROJECT_HOME/software/conda3
-export TENSORFLOW_HOME=$PROJECT_HOME/software/tensorflow-1.0
 
+export JUPYTER_HOME=$PROJECT_HOME/software/jupyter
 export JUPYTER_DATA_DIR=$PROJECT_HOME/software/jupyter_data
 
-export PATH=$CONDA_HOME/bin:$TENSORFLOW_HOME/bin:$PATH
+export TENSORFLOW_HOME=$PROJECT_HOME/software/tensorflow-1.0
+
+export PATH=$JUPYTER_HOME/bin:$TENSORFLOW_HOME/bin:$PATH
--- setup-mac.sh
+++ setup-mac.sh
@@ -6,28 +6,29 @@ source conf/env.sh
 
 # Python install
 
-rm -rf $CONDA_HOME
+rm -rf $JUPYTER_HOME
 
 curl -k -L -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-bash Miniconda3-latest-MacOSX-x86_64.sh -b -f -p $CONDA_HOME
+bash Miniconda3-latest-MacOSX-x86_64.sh -b -f -p $JUPYTER_HOME
 rm -f Miniconda3-latest-MacOSX-x86_64.sh
 
-$CONDA_HOME/bin/conda update -y conda
-$CONDA_HOME/bin/conda update -y pip
+$JUPYTER_HOME/bin/conda update -y conda
+$JUPYTER_HOME/bin/conda update -y pip
 
 rm -rf $JUPYTER_DATA_DIR
 mkdir -p $JUPYTER_DATA_DIR
-$CONDA_HOME/bin/conda install -y jupyter
+$JUPYTER_HOME/bin/conda install -y jupyter
 
 # TensorFlow 1.0 (TensorBoard)
 
 rm -rf $TENSORFLOW_HOME
 
-$CONDA_HOME/bin/conda create -y -p $TENSORFLOW_HOME python=3.6
-$TENSORFLOW_HOME/bin/conda install -y ipykernel
-$TENSORFLOW_HOME/bin/conda install -y scikit-learn
-$TENSORFLOW_HOME/bin/conda install -y matplotlib
+$JUPYTER_HOME/bin/conda create -y -p $TENSORFLOW_HOME python=3.6
 $TENSORFLOW_HOME/bin/pip install tensorflow==1.0.0
+$TENSORFLOW_HOME/bin/pip install ipykernel
+$TENSORFLOW_HOME/bin/pip install scikit-learn
+$TENSORFLOW_HOME/bin/pip install matplotlib
+$TENSORFLOW_HOME/bin/pip install scipy
 
 mkdir -p $JUPYTER_DATA_DIR/kernels/tensorflow-1.0-py3
 
