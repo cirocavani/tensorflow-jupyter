58a9e21eb8f07ef86fded90b275b356c5d5ffd27
Ciro Cavani <ciro.cavani@gmail.com>
2017-02-20 23:03:41 -0300

    Instalação de pacotes com Conda

--- setup-linux.sh
+++ setup-linux.sh
@@ -9,26 +9,25 @@ source conf/env.sh
 rm -rf $CONDA_HOME
 
 curl -k -L -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p $CONDA_HOME
 rm -f Miniconda3-latest-Linux-x86_64.sh
 
-$CONDA_HOME/bin/pip install --upgrade pip
+$CONDA_HOME/bin/conda update -y conda
+$CONDA_HOME/bin/conda update -y pip
 
 rm -rf $JUPYTER_DATA_DIR
 mkdir -p $JUPYTER_DATA_DIR
-$CONDA_HOME/bin/pip install jupyter
+$CONDA_HOME/bin/conda install -y jupyter
 
 # TensorFlow 1.0 (TensorBoard)
 
 rm -rf $TENSORFLOW_HOME
 
 $CONDA_HOME/bin/conda create -y -p $TENSORFLOW_HOME python=3.6
-$TENSORFLOW_HOME/bin/pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-cp36-cp36m-linux_x86_64.whl
-$TENSORFLOW_HOME/bin/pip install ipykernel
-$TENSORFLOW_HOME/bin/pip install matplotlib
-$TENSORFLOW_HOME/bin/pip install sklearn
-$TENSORFLOW_HOME/bin/pip install scipy
+$TENSORFLOW_HOME/bin/conda install -y ipykernel
+$TENSORFLOW_HOME/bin/conda install -y scikit-learn
+$TENSORFLOW_HOME/bin/conda install -y matplotlib
+$TENSORFLOW_HOME/bin/pip install tensorflow==1.0.0
 
 mkdir -p $JUPYTER_DATA_DIR/kernels/tensorflow-1.0-py3
 
