ae4851478a8c80854215acc1fb28f52f5b7602f6
Ciro Cavani <ciro.cavani@gmail.com>
2017-06-16 19:28:39 -0300

    TensorFlow 1.2 + cleanups

--- setup.sh
+++ setup.sh
@@ -13,10 +13,10 @@ PLATFORM="$(uname -s | tr 'A-Z' 'a-z')"
 
 case $PLATFORM in
     linux)
-        CONDA_PKG=Miniconda3-4.3.21-Linux-x86_64.sh
+        CONDA_PKG=Miniconda3-4.3.22-Linux-x86_64.sh
         ;;
     darwin)
-        CONDA_PKG=Miniconda3-4.3.21-MacOSX-x86_64.sh
+        CONDA_PKG=Miniconda3-4.3.22-MacOSX-x86_64.sh
         ;;
     *)
         echo "Unsupported platform: $PLATFORM"
