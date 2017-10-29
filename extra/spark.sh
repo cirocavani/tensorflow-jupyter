e1194c9c0f59ffbad50134de7ece388a5214a044
Ciro Cavani <ciro.cavani@gmail.com>
2017-10-28 22:25:35 -0200

    Spark 2.1.1 -> 2.2.0

--- extra/spark.sh
+++ extra/spark.sh
@@ -4,7 +4,10 @@ set -eu
 cd $(dirname "$0")/..
 source conf/env.sh
 
-SPARK_HOME=$PROJECT_HOME/software/spark-2.1.1-bin-hadoop2.7
+SPARK_VERSION=2.2.0
+SPARK_BIN_NAME=spark-$SPARK_VERSION-bin-hadoop2.7
+SPARK_PKG=$SPARK_BIN_NAME.tgz
+SPARK_HOME=$PROJECT_HOME/software/$SPARK_BIN_NAME
 PYSPARK_HOME=$PROJECT_HOME/software/pyspark
 PYSPARK_KERNEL=$JUPYTER_DATA_DIR/kernels/pyspark
 
@@ -12,11 +15,15 @@ rm -rf $SPARK_HOME
 rm -rf $PYSPARK_HOME
 rm -rf $PYSPARK_KERNEL
 
-curl -k -L -O http://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz
+if [ ! -f .cache/$SPARK_PKG ]; then
+    curl -k -L \
+        http://d3kbcqa49mib13.cloudfront.net/$SPARK_PKG \
+        -o .cache/$SPARK_PKG
+fi
+
 
 mkdir -p $SPARK_HOME
-tar zxf spark-2.1.1-bin-hadoop2.7.tgz --strip-components=1 -C $SPARK_HOME
-rm -rf spark-2.1.1-bin-hadoop2.7.tgz
+tar zxf .cache/$SPARK_PKG --strip-components=1 -C $SPARK_HOME
 
 $CONDA_HOME/bin/conda create -y -p $PYSPARK_HOME python=3.6
 $PYSPARK_HOME/bin/conda install -y -p $PYSPARK_HOME ipykernel
